44a48415ea07a44663eacaae58f0e7f76d5f6c43
kirillkaritskiy <121603995+kirillkaritskiy@users.noreply.github.com>
2022-12-29 11:18:11 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -3,13 +3,19 @@ using namespace std;
 
 int main()
 {
-int k, i = 1, sum_k = 0, number;
+int k, i = 2, sum_k = 0, number;
 cout << " Enter the amount of integers to sum = "; cin >> k;
+if (k==0)
+cout<<" it impossible if the number of int is zero :"<<endl;
+else
+{
+
 do {
 cout << "Enter integer nr. " << i << ": "; 
 cin >> number;
 sum_k += number; i++;
 } while (i <= k);
 cout << " The total sum of " << k << " integers is: "  << sum_k;
+}
 return 0;
 }
